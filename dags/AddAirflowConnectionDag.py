7ff4029ff9eb1051bcb735442d6086e92fa7c8ee
unknown <naranneuron@gmail.com>
2024-07-25 18:09:34 -0700

    Changes to Airflow Connection DAG

--- dags/AddAirflowConnectionDag.py
+++ dags/AddAirflowConnectionDag.py
@@ -6,61 +6,54 @@ from airflow.models import Connection
 import json
 
 def add_connection(**kwargs):
-    
-    # thee connection 
+    # Create a new connection object
     new_conn = Connection(
         conn_id='google_cloud_default',
         conn_type='google_cloud_platform'
     )
 
-    # metadata on the connection: scope, project, key 
+    # Define connection details
     conn_details = {
-        # api interface
         'extra__google_cloud_platform__scope': 'https://www.googleapis.com/auth/cloud-platform',
         'extra__google_cloud_platform__project': project_name,
         'extra__google_cloud_platform__key_path': path_to_json
     }
 
-
     session = settings.Session()
-    # check if the connection exists
-    if session.query(Connection).filter(Connection.conn_id == new_conn.conn_id).first():
-        # query a connection, return the connection w the same ID as the last
-        my_connection = session.query(Connection).filter(Connection.conn_id == new_conn.conn_id).one()
-        
-        # update connection details just for consistency
-        my_connection.set_extra(json.dumps(conn_details))
-        session.commit()
-    
-    # if the connection doesnt exist
+
+    # Check if the connection already exists
+    existing_conn = session.query(Connection).filter(Connection.conn_id == new_conn.conn_id).first()
+    if existing_conn:
+        # Update connection details if it exists
+        existing_conn.set_extra(json.dumps(conn_details))
     else:
-        # set it to the connection above for creation 
+        # Add new connection if it does not exist
         new_conn.set_extra(json.dumps(conn_details))
         session.add(new_conn)
-        session.commit()
+    
+    session.commit()
 
-
-
-
-# DAG details
+# Define default arguments for the DAG
 default_args = {
-    'owner' : 'airflow',
-    'depends_on_past' : False,
-    'start_date' : datetime(2024, 3, 7), 
-    'email' : ['airflow@airflow.com'],
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 3, 7),
+    'email': ['airflow@airflow.com'],
     'email_on_failure': False,
     'email_on_retry': False,
-    'retries' : 3,
+    'retries': 3,
     'retry_delay': timedelta(minutes=5)
 }
 
+# Initialize the DAG
 dag = DAG('AddGCPConnection', default_args=default_args, schedule_interval='@once')
 
-with dag: 
-    activateGCP = PythonOperator(
-        task_id ='airflow_to_gcp_conn', 
+# Define tasks within the DAG
+with dag:
+    connect_to_gcs = PythonOperator(
+        task_id='airflow_to_gcp_conn',
         python_callable=add_connection,
         provide_context=True
     )
 
-activateGCP
+connect_to_gcs
